c2c02fc12b90208c09b5d7288c703d18ef15c936
adilgereev <57193512+adilgereev@users.noreply.github.com>
2021-11-15 16:20:05 +0300

    Добавил вспомогательный класс contact, передал его параметры метода

---
+++ contact.py
@@ -0,0 +1,11 @@
+
+
+class Contact:
+    def __init__(self, firstname, middlename, nickname, company, address, phone_num, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.phone_num = phone_num
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -11,8 +12,9 @@ class TestAddContact(unittest.TestCase):
     def test_untitled_test_case(self):
         wd = self.wd
         self.open_hone_page(wd)
-        self.login(wd)
-        self.create_contact(wd)
+        self.login(wd, username='admin', password="secret")
+        self.create_contact(wd, Contact(firstname=u"Шарап", middlename=u"Адильгереев", nickname="Adil", company="MRM",
+                            address=u"Москва", phone_num="+7926", email="adilgereev05@ya.ru"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -22,40 +24,40 @@ class TestAddContact(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home").click()
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, contact):
         # init create creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(u"Шарап")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(u"Адильгереев")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Adil")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("MRM")
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(u"Москва")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+7926")
+        wd.find_element_by_name("mobile").send_keys(contact.phone_num)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("adilgereev05@ya.ru")
+        wd.find_element_by_name("email").send_keys(contact.email)
         # submit group creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys('admin')
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_hone_page(self, wd):
