16ead502daffedb4da6a26fc6e800df897774763
adilgereev <57193512+adilgereev@users.noreply.github.com>
2021-11-19 10:19:43 +0300

    Перевел test_add_contact на библиотеку pytest, добавил класс-помощник contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,71 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from model.contact import Contact
+from fixture.application import Application
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(5)
-    
-    def test_untitled_test_case(self):
-        wd = self.wd
-        self.open_hone_page(wd)
-        self.login(wd, username='admin', password="secret")
-        self.create_contact(wd, Contact(firstname=u"Шарап", middlename=u"Адильгереев", nickname="Adil", company="MRM",
-                            address=u"Москва", phone_num="+7926", email="adilgereev05@ya.ru"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home").click()
-
-    def create_contact(self, wd, contact):
-        # init create creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.phone_num)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # submit group creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_hone_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.open_home_page()
+    app.session.login(username='admin', password="secret")
+    app.contact.create(Contact(firstname=u"Шарап", middlename=u"Адильгереев", nickname="Adil", company="MRM",
+                               address=u"Москва", phone_num="+7926", email="adilgereev05@ya.ru"))
+    app.return_to_home_page()
+    app.session.logout()
