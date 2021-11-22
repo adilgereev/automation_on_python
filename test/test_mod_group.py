c6d7d5f73c6c8da32024a79c48cf1866db14d9e4
adilgereev <57193512+adilgereev@users.noreply.github.com>
2021-11-22 23:49:48 +0300

    Изменил тесты модификации групп на использование в них условий (if-then) Также уменьшено дублирование кода

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,9 +1,13 @@
 from model.group import Group
 
 
-def test_mod_first_group(app):
+def test_mod_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="name modify",
-                                       header="header modify",
-                                       footer="footer modify"))
+    app.group.modify_first_group(Group(name="name modify"))
+    app.session.logout()
+
+
+def test_mod_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="header modify"))
     app.session.logout()
