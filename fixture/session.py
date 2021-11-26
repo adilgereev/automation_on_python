8ed39196ecfdb1dce10bec8775b49ee1983f92c8
adilgereev <57193512+adilgereev@users.noreply.github.com>
2021-11-26 22:07:59 +0300

    Изменил поиск локатора в is_logged_in

--- fixture/session.py
+++ fixture/session.py
@@ -16,7 +16,7 @@ class SessionHelper:
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_elements_by_xpath("//input[@value='Logout']")) > 0
+        return len(wd.find_elements_by_link_text("Logout")) > 0
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
